f2c0fdce9ca1bf0f04b297cc0013c19fc3daa12a
Hythodev <hytholodium2021@gmail.com>
2023-02-16 05:12:26 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu bytes\n", sizeof(char));
+	printf("Size of a char: %lu bytes\n", sizeof(int));
+	printf("Size of a char: %lu bytes\n", sizeof(long int));
+	printf("Size of a char: %lu bytes\n", sizeof(long long int));
+	printf("Size of a char: %lu bytes\n", sizeof(float));
+	return (0);
+}
