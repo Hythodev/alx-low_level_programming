92a0d86e63e8a21eedb6954a5645dfac0e6602e2
Hythodev <hytholodium2021@gmail.com>
2023-02-17 06:25:50 -0800

    letter values

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - program entry
+ *
+ * Description: assigns n a random number each time it's executed
+ * Return: whether n is positive, zero or negative
+ */
+int main(void)
+{
+	int l, c;
+
+	l = 'z';
+
+	while (l >= 'a')
+	{
+		putchar(l);
+		l--;
+	}
+	putchar('\n');
+	return (0);
+}
