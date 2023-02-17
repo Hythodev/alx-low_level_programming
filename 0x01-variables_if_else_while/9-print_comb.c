aa0277b52f20ba787cfb1ab1851aae5d821ff535
Hythodev <hytholodium2021@gmail.com>
2023-02-17 06:52:45 -0800

    letters

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - program entry
+ *
+ * Description: assigns n a random number each time it's executed
+ * Return: whether n is positive, zero or negative
+ */
+int main(void)
+{
+	int l;
+
+	l = 48;
+
+	while (l <= 57)
+	{
+		putchar(l);
+		putchar(',');
+		putchar(' ')
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
