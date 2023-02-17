d21c0c7891a9ae9626304b0f1e008c34466257a1
Hythodev <hytholodium2021@gmail.com>
2023-02-17 05:31:19 -0800

    letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - program entry
+ *
+ * Description: assigns n a random number each time it's executed
+ * Return: whether n is positive, zero or negative
+ */
+int main(void)
+{
+	int l = 'a';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+
+	int c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
