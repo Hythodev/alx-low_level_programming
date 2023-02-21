f6a454ab562db734641d3838245513223c91628a
Hythodev <hytholodium2021@gmail.com>
2023-02-21 14:15:27 -0800

    letters

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet_x10 - print letters 10 times
+ *
+ * Description: print letters
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int l, i;
+	i = 10;
+	while (i < 10)
+	{
+		l = 'a';
+
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+	}
+	i++;
+	_putchar('\n');
+}
