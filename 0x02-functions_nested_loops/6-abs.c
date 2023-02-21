50cf59918e7fe80a695ba4d91dfe480850c575d2
Hythodev <hytholodium2021@gmail.com>
2023-02-21 15:19:41 -0800

    value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _abs - value
+ * @int: int
+ *
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+
+	return (0);
+}
