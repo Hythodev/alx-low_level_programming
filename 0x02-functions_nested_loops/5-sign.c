43e29c8b0c9f2f3765dd68a6fd3ca360a709aa1e
Hythodev <hytholodium2021@gmail.com>
2023-02-21 15:13:41 -0800

    value

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_sign - check value
+ * @n: int
+ *
+ * Return: int
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0)
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+	return (0);
+}
