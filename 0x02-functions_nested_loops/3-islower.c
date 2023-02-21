355f85436b11a20e92941289428277901537e131
Hythodev <hytholodium2021@gmail.com>
2023-02-21 14:38:17 -0800

    if lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet - program entry
+ *
+ * Description: print letters
+ * Return: void
+ */
+int _islower(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	if(c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
