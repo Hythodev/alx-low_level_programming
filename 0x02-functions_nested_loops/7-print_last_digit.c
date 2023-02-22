3d5a07c7d9ed6be65dc0ee6350979a74322880de
Hythodev <hytholodium2021@gmail.com>
2023-02-21 16:09:53 -0800

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,35 +17,45 @@ int print_last_digit(int n)
 	{
 	case 0:
 		_putchar(48);
+		return (l);
 		break;
 	case 1:
                 _putchar(49);
+		return (l);
                 break;
 	case 2:
 		_putchar(50);
+		return (l);
 		break;
 	case 3:
 		_putchar(51);
+		return (l);
 		break;
 	case 4:
 		_putchar(52);
+		return (l);
 		break;
 	case 5:
 		_putchar(53);
+		return (l);
 		break;
 
 	case 6:
 		_putchar(54);
+		return (l);
 		break;
 
 	case 7:
 		_putchar(55);
+		return (l);
 		break;
 	case 8:
 		_putchar(56);
+		return (l);
 		break;
 	case 9:
 		_putchar(57);
+		return (l);
 		break;
 	}
 
