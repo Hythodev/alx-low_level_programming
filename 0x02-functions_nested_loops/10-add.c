dac452bca5daada3cf9b665d489f6472e1502adc
Hythodev <hytholodium2021@gmail.com>
2023-02-22 02:21:26 -0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * add - add values
+ * @n: int
+ * @m: int
+ *
+ * Return: int
+ */
+int add(int n, int m)
+{
+	int sum;
+	sum = n+m;
+
+	return (sum);
+}
