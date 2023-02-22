098ed1b7cf6fbf66c5518a3532e616f2e85f674a
Hythodev <hytholodium2021@gmail.com>
2023-02-22 04:11:22 -0800

    nums

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * write_num - add values
+ * @num: int
+ *
+ * Return: void
+ */
+void write_num(int num)
+{
+	if (num / 10)
+		write_num(num / 10);
+
+	_putchar(num % 10 + '0');
+}
+
+
+/**
+ * times_table - add values
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int n
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		for (int i = 0; i <= 9; i++)
+		{
+			int prod;
+
+			prod = n * i;
+
+			write_num(prod);
+			
+			if (i != 9)
+				_putchar(44);
+		}
+		n++;
+		_putchar('\n');;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,3 +18,4 @@ void jack_bauer(void);
 int add(int, int);
 void write_num(int num);
 void print_to_98(int n);
+void times_table(void);
