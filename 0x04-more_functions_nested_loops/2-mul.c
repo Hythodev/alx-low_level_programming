ad33194fa8e16f518b81578b3699c4e24afbf583
Hythodev <hytholodium2021@gmail.com>
2023-02-23 09:56:15 -0800

    prod

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * mul - returns prod of two intss
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
