7bec4bda47e08d14234a558c8ad04935e0e9dd5c
Hythodev <hytholodium2021@gmail.com>
2023-02-23 10:04:48 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_numbers - numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
