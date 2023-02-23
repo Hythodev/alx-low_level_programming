c3304e2c8dcff1abd5a49c81dc1bfcd6493aeadc
Hythodev <hytholodium2021@gmail.com>
2023-02-23 09:41:18 -0800

    digit

---
+++ 0x04-more_functions_nested_loops/1-digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _isdigit - returns 1 if digit otherwise 0
+ * @c: int character value
+ *
+ * Description: print letters
+ * Return: int
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
