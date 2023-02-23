02e530dfc73c95682728310685b408452249ad7b
Hythodev <hytholodium2021@gmail.com>
2023-02-23 10:44:33 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * more_numbers - numbers
+ *
+ * Return: void
+ */
+void write_num(int num)
+{
+	if (num / 10)
+		write_num(n / 10);
+	_putchar(num % 10 + '0');
+}
+
+/**
+ * more_numbers - numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+        i = 0;
+
+	while (j < 10)
+	{
+		j = 0;
+
+		while (i <= 14)
+		{
+			write_num(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
