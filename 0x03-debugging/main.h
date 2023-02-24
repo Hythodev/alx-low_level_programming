6e0b82ba479eded60a868dc274b7cda73b761463
Hythodev <hytholodium2021@gmail.com>
2023-02-23 16:19:49 -0800

    fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
 
-/**
- * _putchar - writes c to output
- *@c: char to write
- *
- * Return: c
- */
 void positive_or_negative(int i);
-int main(void);
+
+#endif /* MAIN_H */
