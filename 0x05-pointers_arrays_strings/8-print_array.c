ce67599f9a38c0aebc2570e93b6d198675b9bbcc
Hythodev <hytholodium2021@gmail.com>
2023-02-28 10:39:43 -0800

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_array - int length
+ * @a: array param
+ * @n: int
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n -1)
+		{
+			_putchar(46);
+			_putchar(32);
+		}
+	}
+	_putchar('\n');
+}
