03f8f67b8aebed1713c001bd75f728b4dc737b16
Hythodev <hytholodium2021@gmail.com>
2023-02-28 09:59:16 -0800

    skipping

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * puts2 - int length
+ * @str: char param
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int l, i;
+
+	for (l = 0; str[l] != '\0'; ++l)
+	;
+
+	for (i = l - 1; i >= 0; i--)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
