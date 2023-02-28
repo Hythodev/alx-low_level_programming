18abd2b590587ba04453fe4f786e996fe0c1e1af
Hythodev <hytholodium2021@gmail.com>
2023-02-28 08:39:25 -0800

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strlen - int length
+ * @s: int param
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+
+	return l;
+}
