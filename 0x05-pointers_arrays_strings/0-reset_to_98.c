42a8e12aab44edc2e16f5e6159867abb804bedba
Hythodev <hytholodium2021@gmail.com>
2023-02-28 08:09:05 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - reassign
+ * @n: int param
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes c to output
+ *@c: char to write
+ *
+ * Return: c
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
