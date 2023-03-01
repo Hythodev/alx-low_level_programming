6d8e8c2eb31d72c58cb5968f16e8df835508adbf
Hythodev <hytholodium2021@gmail.com>
2023-03-01 14:20:14 -0800

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * string_toupper - string
+ * @s: string param
+ *
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return ();
+}
