d52e0f5c4eb73f7dc6a4fb744dbe39934531d71c
Hythodev <hytholodium2021@gmail.com>
2023-03-01 15:56:15 -0800

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,12 +11,12 @@ char *cap_string(char *str) {
 
 	int i, n;
 
-	for(n =0; str[n] != '\0'; ++n)
+	for (n = 0; str[n] != '\0'; ++n)
 	;
 
-	n = n +1;
+	n = n + 1;
 
-	for(i = 0; i<n; i++)
+	for (i = 0; i < n; i++)
 	{
 
 		if (i == 0 && (str[i] != ' ' || str[i] != 9 || str[i] != 10 || str[i] != 44 
@@ -30,7 +30,7 @@ char *cap_string(char *str) {
 		str[i-1] == 40) || (str[i] != 41 && str[i-1] == 41) || (str[i] != 123 && 
 		str[i-1] == 123) || (str[i] != 125 && str[i-1] == 125)))
 		{
-			if(str[i] >= 'a' && str[i] <= 'z')
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32;
 			}
