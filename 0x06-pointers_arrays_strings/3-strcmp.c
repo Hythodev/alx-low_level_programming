ee1c08df25a7bcdbcbb6df810ef35c9c17f3b30f
Hythodev <hytholodium2021@gmail.com>
2023-03-01 13:46:02 -0800

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strcmp - string
+ * @s1: char param
+ * @s2: char param
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(char *)s1 - *(char*)s2);
+}
