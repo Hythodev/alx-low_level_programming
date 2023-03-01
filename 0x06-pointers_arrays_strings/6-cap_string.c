c47a7c315a43f11ce795449de7e356332fe04a8b
Hythodev <hytholodium2021@gmail.com>
2023-03-01 15:01:15 -0800

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * cap_string - string
+ * @s: string param
+ *
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i, n;
+
+	for (n = 0; str[n] != '\0'; ++n)
+	;
+
+	n = n + 1;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0 && (str[i] != ' ' || str[i] != 9 || str[i] != 10 || str[i] != 44 
+		|| str[i] != 59 || str[i] != 46 || str[i] != 33 || str[i] != 63 || 
+		str[i] != 34 || str[i] != 40 || str[i] != 41) || str[i] != ' ' && 
+		str[i-1] == ' ' || str[i] != 9 && str[i-1] == 9 || str[i] != 10 && 
+		str[i-1] == 10 || str[i] != 44 && str[i-1] == 44 || str[i] != 59 && 
+		str[i-1] == 59 || str[i] != 46 && str[i-1] == 46 || str[i] != 33 && 
+		str[i-1] == 33 || str[i] != 63 && str[i-1] == 63 || str[i] != 34 && 
+		str[i-1] == 34 || str[i] != 40 && str[i-1] == 40 || str[i] != 41 && 
+		str[i-1] == 41)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = (char)(('A' - 'a') + str[i]);
+			else if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = (char)(str[i] + ('a' - 'A'));
+		}
+	}
+	return (s);
+}
