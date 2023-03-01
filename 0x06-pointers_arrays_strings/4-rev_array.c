f7d642b77b3ef514115a671581888246a4f8ed1f
Hythodev <hytholodium2021@gmail.com>
2023-03-01 13:59:01 -0800

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reverse_array - array
+ * @a:array  param
+ * @n: int param
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n/2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
