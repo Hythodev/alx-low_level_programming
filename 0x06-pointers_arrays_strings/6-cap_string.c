a3a94a5ecb30039b9917dabcbdeef732a625bb2e
Hythodev <hytholodium2021@gmail.com>
2023-03-01 15:51:37 -0800

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,36 +7,38 @@
  *
  * Return: string
  */
-char *cap_string(char *str)
-{
+char *cap_string(char *str) {
+
 	int i, n;
 
-	for (n = 0; str[n] != '\0'; ++n)
+	for(n =0; str[n] != '\0'; ++n)
 	;
 
-	n = n + 1;
+	n = n +1;
 
-	for (i = 0; i < n; i++)
+	for(i = 0; i<n; i++)
 	{
+
 		if (i == 0 && (str[i] != ' ' || str[i] != 9 || str[i] != 10 || str[i] != 44 
 		|| str[i] != 59 || str[i] != 46 || str[i] != 33 || str[i] != 63 || 
-		str[i] != 34 || str[i] != 40 || str[i] != 41) || 
-		((str[i] != ' ' && str[i-1] == ' ') || (str[i] != 9 && str[i-1] == 9) || 
-		 (str[i] != 10 && str[i-1] == 10) || (str[i] != 44 && str[i-1] == 44) || 
-		 (str[i] != 59 && str[i-1] == 59) || (str[i] != 46 && str[i-1] == 46) || 
-		 (str[i] != 33 && str[i-1] == 33) || (str[i] != 63 && str[i-1] == 63) || 
-		 (str[i] != 34 && str[i-1] == 34) || (str[i] != 40 && str[i-1] == 40) || 
-		 (str[i] != 41 && str[i-1] == 41)))
+		str[i] != 34 || str[i] != 40 || str[i] != 41 || str[i] != 123 || 
+		str[i] != 125) || ((str[i] != ' ' && str[i-1] == ' ') || (str[i] != 9 && 
+		str[i-1] == 9) || (str[i] != 10 && str[i-1] == 10) || (str[i] != 44 && 
+		str[i-1] == 44) || (str[i] != 59 && str[i-1] == 59) || (str[i] != 46 && 
+		str[i-1] == 46) || (str[i] != 33 && str[i-1] == 33) || (str[i] != 63 && 
+		str[i-1] == 63) || (str[i] != 34 && str[i-1] == 34) || (str[i] != 40 && 
+		str[i-1] == 40) || (str[i] != 41 && str[i-1] == 41) || (str[i] != 123 && 
+		str[i-1] == 123) || (str[i] != 125 && str[i-1] == 125)))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if(str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32;
 			}
-			else if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
-			}
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
 		}
 	}
-	return (str);
+	return str;
 }
