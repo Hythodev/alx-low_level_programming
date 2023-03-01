95ae2753600267d4ca75c6a2caff3f4e869fb1f3
Hythodev <hytholodium2021@gmail.com>
2023-03-01 10:32:09 -0800

    string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strncat - string
+ * @dest: char param
+ * @src: char param
+ * @n: int
+ *
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, k;
+
+	for (l = 0; dest[l] != '\0'; l++)
+	;
+
+	l = l + 1;
+
+	for (k = 0;k < n && src[k] != '\0'; k++)
+	{
+		dest[l + k] = src[k];
+	}
+
+	dest[j + k] = '\0';
+
+	return (dest);
+}
