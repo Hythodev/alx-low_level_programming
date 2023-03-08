dc3784feca8928585e4acfcefc93fb35e4b37948
Hytho <yohytholodiuum2021@gmail.com>
2023-03-07 16:34:30 -0800

    fix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+i#include "main.h"
+/**
+ * _strspn - function
+ * @s: char param
+ * @accept: char param
+ *
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *p;
+
+	int n;
+
+	for (n = 0; *s; s++, n++)
+	{
+		for (p = accept; *p && *p != *s; p++)
+		;
+
+		if (!*p)
+			break;
+	}
+	return (n);
+
+}
