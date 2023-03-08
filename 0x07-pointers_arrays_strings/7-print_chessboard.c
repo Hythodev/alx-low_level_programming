41784f874d9cac28ed7c1cf45edbd956acf339b9
Hytho <yohytholodiuum2021@gmail.com>
2023-03-07 16:54:17 -0800

    board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_chessboard - function
+ * @a: char param
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int l, i, j;
+
+	for (l = 0; *a[l] != '\0'; ++l)
+	;
+
+	l = l + 1;
+
+	for (i = 0; i < l; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+
+}
