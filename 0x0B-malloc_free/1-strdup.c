0033b1a6f5dcee4b12623918eb451ec0e57baf8b
Hytho <yohytholodiuum2021@gmail.com>
2023-03-14 18:44:40 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - function
+ * @str: char param
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char array[];
+
+	array = str;
+
+	if (str == NULL)
+		return (NULL);
+
+	array = malloc(sizeof(str));
+
+	if (array == NULL)
+		return (NULL);
+	else
+	{
+		return (array);
+	}
+	free(array);
+}
