b7443558d979feb422d6880b0212aaf6160c9468
Hytho <yohytholodiuum2021@gmail.com>
2023-03-14 18:18:23 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _create_array - function
+ * @size: int param
+ * @c: char param
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	
+	if (size != 0)
+	{
+		array = malloc(sizeof(char) * size);
+		array[0] = c;
+		return array;
+	}
+	else
+	{
+		return NULL;
+	}
+
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes c to output
+ *@c: char to write
+ *
+ * Return: c
+ */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
