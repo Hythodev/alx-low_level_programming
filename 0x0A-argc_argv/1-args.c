f3122db8991f30895927e62a1d92b81b6fbd6695
Hytho <yohytholodiuum2021@gmail.com>
2023-03-15 10:31:58 -0700

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: count param
+ * @argv: char
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
