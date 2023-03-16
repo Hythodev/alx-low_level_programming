2b04bdeeeb866afc489ad37ee04e688d9057b5e4
Hytho <yohytholodiuum2021@gmail.com>
2023-03-16 16:44:33 -0700

    pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * malloc_checked - function
+ * @b: int param
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int j;
+
+	unsigned int array;
+
+	array = malloc(sizeof(unsigned int));
+
+	if (array == NULL)
+		return (98);
+
+	return (array);
+	free(array);
+}
