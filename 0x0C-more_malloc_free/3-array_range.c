3f99e99b4c6b697763e31e91b28ef47bf78f3371
Hytho <yohytholodiuum2021@gmail.com>
2023-03-17 03:34:37 -0700

    array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * array_range - function
+ * @min: int param
+ * @max: int param
+ *
+ * Return: int
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+
+	int n, i;
+
+	n = (max - min) + 1;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * n);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+	{
+		array[i] = i;
+	}
+	return (array);
+}
