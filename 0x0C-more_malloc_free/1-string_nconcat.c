b71cc6a23160c0ae34e17f22bc4b737794c6bb79
Hytho <yohytholodiuum2021@gmail.com>
2023-03-16 18:18:52 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0 ; i < l'; i++)
+	for (i = 0 ; i < l; i++)
 		array[i] = s1[i];
 	for (k = 0; k < n; k++)
 	{
