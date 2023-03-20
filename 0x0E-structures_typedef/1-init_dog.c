39ebdea791cc4e969642e6be95a07b73635b20e5
Hytho <yohytholodiuum2021@gmail.com>
2023-03-20 11:35:14 -0700

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <dog.h>
 
-/**
- * init_dog - function
- * @d: struct
- * @name: name
- * @age: age
- * @owner: owner
- *
- * Return: void
- */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
