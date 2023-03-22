a33ccce945be0bbebfe045d5344837725ac45eee
Hytho <yohytholodiuum2021@gmail.com>
2023-03-21 18:28:35 -0700

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,6 @@
 void print_dog(struct dog *d)
 {
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Age: %lf\n", d->age != NULL ? d->age : "(nil)");
+	printf("Age: %lf\n", d->age != 0 ? d->age : 0);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner: "((nil)");
 }
