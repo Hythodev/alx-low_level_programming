f349842befcfc25298547c3589bfefa9ae74759a
Hytho <yohytholodiuum2021@gmail.com>
2023-03-30 15:30:23 -0700

    linkedlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/**
+ * print_list - function
+ * @h: array param
+ *
+ * Return: int
+ */
+int print_list(const list_t *h)
+{
+	int l, i;
+
+	l = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			for (i = 0; h->str[i] != '\0'; i++)
+			;
+
+			printf("[%d} %s", i, h->str);
+		}
+		else
+			printf("[%d] %s", 0, "(nil)");
+
+		h = h->next;
+		l++;
+		printf("\n");
+	}
+	return (l);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes c to output
+ *@c: char to write
+ *
+ * Return: c
+ */
+int _putchar(char c);
+size_t print_list(const list_t *h);
